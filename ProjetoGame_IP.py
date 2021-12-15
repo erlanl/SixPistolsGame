5b62bfe953a907aa40be7b00234186e1830f2cca
Pedro Henrique <boahorapedro@gmail.com>
2021-12-14 23:23:12 -0300

    Sistema de vida atualizado

--- ProjetoGame_IP.py
+++ ProjetoGame_IP.py
@@ -58,7 +58,7 @@ class Tiro:
         self.rect.center=[self.x,self.y]
         pg.draw.rect(self.win, self.cor, self.rect)
     
-    def colisao(self, obj, colidor):
+    def colisao_bala(self, obj, colidor):
         return obj.rect.colliderect(colidor)
 
     def movimento(self, vel, direcao):
@@ -73,7 +73,7 @@ class Tiro:
 class Player:
     COOLDOWN = 30 # Metade de um segundo pois o jogo é 60 fps
 
-    def __init__(self, win, x, y, tecla_cima, tecla_baixo, tecla_esquerda, tecla_direita, tecla_tiro, obj):
+    def __init__(self, win, x, y, tecla_cima, tecla_baixo, tecla_esquerda, tecla_direita, tecla_tiro, inimigo):
 
         self.win = win
         self.x = x
@@ -97,7 +97,7 @@ class Player:
         self.cool_down = 0
         self.vida = 100
 
-        self.inimigo = obj
+        self.inimigo = inimigo
         self.balas = 0
 
     # Funcao que vai verificar se o player colidiu com uma plataforma
@@ -203,7 +203,7 @@ class Player:
         self.cooldown()
         for bala in self.tiros:
             bala.movimento(vel, self.tecla_tiro)
-            if bala.colisao(bala, obj):
+            if bala.colisao_bala(bala, obj):
                 self.tiros.remove(bala)
                 self.vida -= 10
                 print(self.vida)
@@ -261,9 +261,10 @@ def main():
 
     player1 = Player(screen, 320, 240, pg.K_w, pg.K_s, pg.K_a, pg.K_d, pg.K_f, None)
     player2 = Player(screen, 220, 140, pg.K_UP, pg.K_DOWN, pg.K_LEFT, pg.K_RIGHT, pg.K_RCTRL, None)
+    player1.inimigo = player2
+    player2.inimigo = player1
     
-    player1 = Player(screen, 320, 240, pg.K_w, pg.K_s, pg.K_a, pg.K_d, pg.K_f, player2)
-    player2 = Player(screen, 220, 140, pg.K_UP, pg.K_DOWN, pg.K_LEFT, pg.K_RIGHT, pg.K_RCTRL, player1)
+    
 
     #as fontes estão na pasta Assets, pode escolher qualquer fonte com tipo de arquivo .ttf
     #escolhendo a fonte pra usar, o segundo argumento é o tamanho
